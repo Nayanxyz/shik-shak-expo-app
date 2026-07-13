7a1ed201ea67e869b5429c3a1b26bd3a94a5037e
NayanPython <choudharyji527@gmail.com>
2026-07-13 23:32:02 +0530

    imports

---
+++ lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+import * as Linking from 'expo-linking';
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+
